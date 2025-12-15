97a6800cd0e9c4fc47de8643fd53c08c37b53154
ryotakonishi <ryota.konishi@icloud.com>
2025-12-15 19:11:55 +0900

    [add]変数追加

--- Assets/PlayerShot.cs
+++ Assets/PlayerShot.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class PlayerShot : MonoBehaviour
 {
+    [SerializeField] float shotInterval;
+    [SerializeField] GameObject bullet;
+
+    private ObjectPool<Bullet> bulletPool;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
