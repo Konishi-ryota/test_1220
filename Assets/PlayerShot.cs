aa179c130352b78838592de47ae0e6744b3fb9f7
ryotakonishi <ryota.konishi@icloud.com>
2025-12-16 14:44:18 +0900

    [add]色々追加

--- Assets/PlayerShot.cs
+++ Assets/PlayerShot.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
-
 public class PlayerShot : MonoBehaviour
 {
     [SerializeField] float shotInterval;
-    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject muzzle;
 
     private float _timer;
     private ObjectPool _bulletPool;
@@ -20,6 +19,17 @@ public class PlayerShot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) && _timer < Time.time)
+        {
+            Shot();
+            _timer = Time.time + shotInterval;
+        }
+    }
+    
+    private void Shot()
+    {
+        Bullet bullet = _bulletPool.GetPooledObject();
+        bullet.transform.position = muzzle.transform.position;
+        bullet.transform .rotation = muzzle.transform.rotation;
     }
 }
